f658b548e523ef9789a57fa13dc1b2f743a645e2
Wandercrazy <wandercrazy15@gmail.com>
2021-08-11 18:36:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import './App.css';
+import { CardList } from './components/card-list/card-list.component'
+
+import { SearchBox } from './components/search-box/search-box.component';
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: ''
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase())
+    );
+    return (
+      <div className='App'>
+
+        <SearchBox placeholder='Search Monsters' handleChange={e => this.setState({ searchField: e.target.value })} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+}
+export default App;
